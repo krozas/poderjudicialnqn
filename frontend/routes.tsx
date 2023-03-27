3ca118f8813b326356f4686a0f6f05cd4a11805f
krozas <rozas.karina@gmail.com>
2023-03-27 15:47:39 -0300

    Update routes.tsx

--- frontend/routes.tsx
+++ frontend/routes.tsx
@@ -1,4 +1,3 @@
-import HelloReactView from 'Frontend/views/helloreact/HelloReactView.js';
 import MainLayout from 'Frontend/views/MainLayout.js';
 import { lazy } from 'react';
 import { createBrowserRouter, IndexRouteObject, NonIndexRouteObject, useMatches } from 'react-router-dom';
