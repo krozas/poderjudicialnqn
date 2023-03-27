b51a96a2b0caf19c6b58cc4b16ebb84be0a9317e
krozas <rozas.karina@gmail.com>
2023-03-27 13:09:29 -0300

    Update routes.tsx

--- frontend/routes.tsx
+++ frontend/routes.tsx
@@ -2,6 +2,9 @@ import HelloReactView from 'Frontend/views/helloreact/HelloReactView.js';
 import MainLayout from 'Frontend/views/MainLayout.js';
 import { lazy } from 'react';
 import { createBrowserRouter, IndexRouteObject, NonIndexRouteObject, useMatches } from 'react-router-dom';
+import EdificioView from 'Frontend/views//edificio/EdificioView.js';
+import DependenciaView from './views/dependencia/DependenciaView.js';
+
 
 const AboutView = lazy(async () => import('Frontend/views/about/AboutView.js'));
 export type MenuProps = Readonly<{
@@ -33,8 +36,10 @@ export const routes: readonly ViewRouteObject[] = [
     element: <MainLayout />,
     handle: { icon: 'null', title: 'Main' },
     children: [
-      { path: '/', element: <HelloReactView />, handle: { icon: 'globe-solid', title: 'Hello React' } },
-      { path: '/about', element: <AboutView />, handle: { icon: 'file', title: 'About' } },
+     
+      { path: '/', element: <AboutView />, handle: { icon: 'file', title: 'About' } },
+      { path: '/edificio', element: <EdificioView />, handle: { icon: 'building', title: 'Edificio' } },
+      { path: '/dependencia', element: <DependenciaView />, handle: { icon: 'city-solid', title: 'Dependencia' } },
     ],
   },
 ];
